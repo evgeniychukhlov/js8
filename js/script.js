a30b9eee72785855d0230087b5b8716b129c2cc7
Evgeniy Chukhlov <evgeniy.chukhlov@gmail.com>
2023-08-11 22:00:08 +0300

    js8
    ready

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let x = 50;
+let y = 10; 
+let sum = x + y;
+let subtraction= x - y; 
+let multiplication = x * y; 
+let division = x / y; 
+let degree_x = x ** 2;
+let degree_y= y ** 2;
+console.log( "додавання", sum );
+console.log( "віднімання", subtraction );
+console.log( "множення", multiplication );
+console.log( "ділення", division );
+console.log("х в 2 ступені", degree_x);
+console.log("y в 2 ступені", degree_y);
